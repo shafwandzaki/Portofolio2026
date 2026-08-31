8cbbbaefd49d022814d53281e2039cf733635b8f
ShafwanDzaki <shafwandzaki19@gmail.com>
2026-08-31 13:37:20 +0700

    tambahan hover di card project

--- resources/views/components/projects.blade.php
+++ resources/views/components/projects.blade.php
@@ -9,42 +9,53 @@
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
             
             <!-- Card 1 -->
-            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2">
-
-                <!-- Gunakan gambar asli Anda pada tag src -->
+            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2 hover:text-blue-300">
                 <img src="#" alt="Project SBiz" class="w-full h-48 object-cover bg-gray-700" />
-
                 <div class="p-6 flex flex-col grow">
                     <p class="text-xl font-bold mb-3 leading-snug">Sbiz - Aplikasi Marketplace Berbasis Mobile</p>
                     <p class="text-gray-300 text-sm leading-relaxed">
                         SBiz merupakan aplikasi marketplace berbasis mobile yang dikembangkan sebagai bentuk pengembangan dari platform SBiz berbasis website yang telah tersedia sebelumnya. Aplikasi ini dirancang untuk memberikan pengalaman berbelanja yang lebih praktis dan mudah diakses melalui perangkat mobile.
                     </p>
-                    <p><a href="https://play.google.com/store/apps/details?id=id.co.sbiz">Lihat</a></p>
+                    <a href="https://play.google.com/store/apps/details?id=id.co.sbiz" target="_blank" rel="noopener noreferrer" class="inline-flex items-center gap-1 text-white hover:text-blue-500 text-sm font-semibold mt-8">
+                        Lihat Selengkapnya
+                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                        </svg>
+                    </a>
                 </div>
             </div>
 
             <!-- Card 2 -->
-            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2">
-
+            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2 hover:text-blue-300">
                 <img src="#" alt="Project UI/UX" class="w-full h-48 object-cover bg-gray-700" />
                 <div class="p-6 flex flex-col grow">
                     <p class="text-xl font-bold mb-3 leading-snug">Desain UI/UX - Aplikasi Marketplace Berbasis Mobile</p>
                     <p class="text-gray-300 text-sm leading-relaxed">
                         Sebuah desian aplikasi marketplace dengan berbagai fitur dibuat menggunakan component dengan figma. Perancangan UI/UX mencakup pembuatan user flow, wireframe, struktur navigasi, layout halaman, hingga desain antarmuka final. Desain dibuat dengan mempertimbangkan kemudahan navigasi & kenyamanan pengguna.
                     </p>
-                    <p><a href="https://www.figma.com/design/CAhdaoEwTV9XCJfOPxH1tZ/SBiz?node-id=0-1&t=vyOb0UbXpR9acvTh-1">Lihat</a></p>
+                    <a href="https://www.figma.com/design/CAhdaoEwTV9XCJfOPxH1tZ/SBiz?node-id=0-1&t=vyOb0UbXpR9acvTh-1" target="_blank" rel="noopener noreferrer" class="inline-flex items-center gap-1 text-white hover:text-blue-500 text-sm font-semibold mt-8">
+                        Lihat Selengkapnya
+                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                        </svg>
+                    </a>
                 </div>
             </div>
 
             <!-- Card 3 -->
-            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2">
+            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2 hover:text-blue-300">
                 <img src="#" alt="Project Jurnal Akademik" class="w-full h-48 object-cover bg-gray-700" />
                 <div class="p-6 flex flex-col grow">
                     <p class="text-xl font-bold mb-3 leading-snug">Jurnal Akademik - Aplikasi Berbasis Mobile</p>
                     <p class="text-gray-300 text-sm leading-relaxed">
-                        Jurnal Akademik merupakan aplikasi pembelajaran siswa berbasis mobile yang dikembangkan sebagai bentuk pengembangan dari platform Jurnal Akademik berbasis website. Aplikasi ini dirancang untuk memudahkakn siswa, guru, dan orang tua untuk memantu perkembangan pembelajaran siswa dengan praktis  melalui perangkat mobile.
+                        Jurnal Akademik merupakan aplikasi pembelajaran siswa berbasis mobile yang dikembangkan sebagai bentuk pengembangan dari platform Jurnal Akademik berbasis website. Aplikasi ini dirancang untuk memudahkakn siswa, guru, dan orang tua untuk memantu perkembangan pembelajaran siswa dengan praktis melalui perangkat mobile.
                     </p>
-                    <p><a href="https://play.google.com/store/apps/details?id=id.schoolmedia.jurnal">Lihat</a></p>
+                    <a href="https://play.google.com/store/apps/details?id=id.schoolmedia.jurnal" target="_blank" rel="noopener noreferrer" class="inline-flex items-center gap-1 text-white hover:text-blue-500 text-sm font-semibold mt-8">
+                        Lihat Selengkapnya
+                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                        </svg>
+                    </a>
                 </div>
             </div>
         </div>
@@ -53,24 +64,36 @@
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-24">
             
             <!-- Card 4 -->
-            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2">
+            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2 hover:text-blue-300">
                 <img src="#" alt="Project SBiz 2" class="w-full h-48 object-cover bg-gray-700" />
                 <div class="p-6 flex flex-col grow">
-                    <p class="text-xl font-bold mb-3 leading-snug">Sbiz - Aplikasi Marketplace Berbasis Mobile</p>
+                    <p class="text-xl font-bold mb-3 leading-snug">SuruhinAja - Tampilan Website Jasa Bersih Rumah</p>
                     <p class="text-gray-300 text-sm leading-relaxed">
-                        SBiz merupakan aplikasi marketplace berbasis mobile yang dikembangkan sebagai bentuk pengembangan dari platform SBiz berbasis website yang telah tersedia sebelumnya. Aplikasi ini dirancang untuk memberikan pengalaman berbelanja yang lebih praktis dan mudah diakses melalui perangkat mobile.
+                        SuruhinAja merupakan website jasa bersih rumah, tampilan web ini dibangun menggunakan HTML, CSS, dan JavaScript. fitur di website ini dapat memesan jasa bersih rumah dari jauh dan dapat mengatur jadwal pemesanan.
                     </p>
+                    <a href="#" target="_blank" rel="noopener noreferrer" class="inline-flex items-center gap-1 text-white hover:text-blue-500 text-sm font-semibold mt-8">
+                        Lihat Selengkapnya
+                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                        </svg>
+                    </a>
                 </div>
             </div>
 
             <!-- Card 5 -->
-            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2">
+            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2 hover:text-blue-300">
                 <img src="#" alt="Project UI/UX 2" class="w-full h-48 object-cover bg-gray-700" />
                 <div class="p-6 flex flex-col grow">
-                    <p class="text-xl font-bold mb-3 leading-snug">Desain UI/UX - Aplikasi Marketplace Berbasis Mobile</p>
+                    <p class="text-xl font-bold mb-3 leading-snug">Web Laundry Expres - Tampilan Website Laundry</p>
                     <p class="text-gray-300 text-sm leading-relaxed">
-                        Sebuah desian aplikasi marketplace dengan berbagai fitur dibuat menggunakan component dengan figma. Perancangan UI/UX mencakup pembuatan user flow, wireframe, struktur navigasi, layout halaman, hingga desain antarmuka final. Desain dibuat dengan mempertimbangkan kemudahan navigasi & kenyamanan pengguna.
+                        Laundry Expres merupakan website laundry yang dikembangkan menggunakan HTML, CSS, dan JavaScript sebagi tampilannya. fitur di website ini dapat memesan dan mengatur jadwal pengambilan cucian ketempat pelanggan dan akan diantar setelah cucian selesai.
                     </p>
+                    <a href="#" target="_blank" rel="noopener noreferrer" class="inline-flex items-center gap-1 text-white hover:text-blue-500 text-sm font-semibold mt-8">
+                        Lihat Selengkapnya
+                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                        </svg>
+                    </a>
                 </div>
             </div>
         </div>
@@ -78,28 +101,40 @@
         <!-- Judul Sub Section -->
         <h2 class="text-3xl font-bold text-start mb-12">Karya Saya Yang Lain</h2>
 
-        <!-- Baris Card 3 -->
+        <!-- Baris Card Lain -->
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
             
-            <!-- Card 6 -->
-            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2">
+            <!-- Card Lain 1 -->
+            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2 hover:text-blue-300">
                 <img src="#" alt="Project Other 1" class="w-full h-48 object-cover bg-gray-700" />
                 <div class="p-6 flex flex-col grow">
-                    <p class="text-xl font-bold mb-3 leading-snug">Sbiz - Aplikasi Marketplace Berbasis Mobile</p>
+                    <p class="text-xl font-bold mb-3 leading-snug">Fotografi</p>
                     <p class="text-gray-300 text-sm leading-relaxed">
-                        SBiz merupakan aplikasi marketplace berbasis mobile yang dikembangkan sebagai bentuk pengembangan dari platform SBiz berbasis website yang telah tersedia sebelumnya. Aplikasi ini dirancang untuk memberikan pengalaman berbelanja yang lebih praktis dan mudah diakses melalui perangkat mobile.
+                        Beberapa karya fotografi saya dengan berbagai macam genre seperti potrait, landscape, streen, moment, dll. juga dengan berbagai mood seperti warm, cool, moody, dll serta dengan berbagai macam style atau gaya visual. 
                     </p>
+                    <a href="#" target="_blank" rel="noopener noreferrer" class="inline-flex items-center gap-1 text-white hover:text-blue-500 text-sm font-semibold mt-8">
+                        Lihat Selengkapnya
+                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                        </svg>
+                    </a>
                 </div>
             </div>
 
-            <!-- Card 7 -->
-            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2">
+            <!-- Card Lain 2 -->
+            <div class="flex flex-col bg-[#151822] border border-[#262b3d] rounded-2xl overflow-hidden transition-transform duration-300 hover:-translate-y-2 hover:text-blue-300">
                 <img src="#" alt="Project Other 2" class="w-full h-48 object-cover bg-gray-700" />
                 <div class="p-6 flex flex-col grow">
-                    <p class="text-xl font-bold mb-3 leading-snug">Desain UI/UX - Aplikasi Marketplace Berbasis Mobile</p>
+                    <p class="text-xl font-bold mb-3 leading-snug">Desain Grafis</p>
                     <p class="text-gray-300 text-sm leading-relaxed">
-                        Sebuah desian aplikasi marketplace dengan berbagai fitur dibuat menggunakan component dengan figma. Perancangan UI/UX mencakup pembuatan user flow, wireframe, struktur navigasi, layout halaman, hingga desain antarmuka final. Desain dibuat dengan mempertimbangkan kemudahan navigasi & kenyamanan pengguna.
+                        Beberapa karya desain grafis saya dengan berbagai macam ukuran dan kreativitas dari potrait, landscape, story, feed, poster, flayer, banner, kolase, dll serta dengan berbagai macam style atau gaya visual.
                     </p>
+                    <a href="#" target="_blank" rel="noopener noreferrer" class="inline-flex items-center gap-1 text-white hover:text-blue-500 text-sm font-semibold mt-8">
+                        Lihat Selengkapnya
+                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                        </svg>
+                    </a>
                 </div>
             </div>
         </div>
